8cd40014d13298def8a4e6b804aef56e4bd4acf5
SABBIR AHMED TALUKDAR <satsabbir11@gmail.com>
2021-06-17 10:59:58 +0600

    navigation bar something dynamic

--- functions.php
+++ functions.php
@@ -10,13 +10,14 @@
 
 function philosophy_theme_setup()
 {
-    load_theme_textdomain("philosophy");
+    load_theme_textdomain("philosophy1");
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
     add_theme_support('html5', array('search-form','comment-list'));
     add_theme_support("post-formates", array("image","gallary","quote","audio","video"));
     add_editor_style("/assets/css/editor-style.css");
-    
+
+    register_nav_menu("topmenu",__("Top Menu","philosophy"));
 }
 add_action("after_setup_theme", "philosophy_theme_setup");
 
@@ -27,7 +28,7 @@ function philosophy_assets()
 	wp_enqueue_style( "base-css", get_theme_file_uri( "/assets/css/base.css" ), null, "1.0" );
 	wp_enqueue_style( "vendor-css", get_theme_file_uri( "/assets/css/vendor.css" ), null, "1.0" );
 	wp_enqueue_style( "main-css", get_theme_file_uri( "/assets/css/main.css" ), null, "1.0" );
-	wp_enqueue_style( "philosophy-css", get_stylesheet_uri(), null, VERSION );
+	wp_enqueue_style( "philosophy1-css", get_stylesheet_uri(), null, VERSION );
 
 	wp_enqueue_script( "modernizr-js", get_theme_file_uri( "/assets/js/modernizr.js" ), null, "1.0" );
 	wp_enqueue_script( "pace-js", get_theme_file_uri( "/assets/js/pace.min.js" ), null, "1.0" );
