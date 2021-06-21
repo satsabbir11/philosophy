2d8a08a55b8edf5fa86f6d693d0e6aeaae6e569f
SABBIR AHMED TALUKDAR <satsabbir11@gmail.com>
2021-06-21 11:16:39 +0600

    featured php add

--- template-parts/common/navigation.php
+++ template-parts/common/navigation.php
@@ -1,8 +1,16 @@
-<a class="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
+<a class="header__toggle-menu" href="#0" title="Menu"><span>
+        <?php
+        _e('Menu','philosophy');
+        ?>
+    </span></a>
 
 <nav class="header__nav-wrap">
 
-    <h2 class="header__nav-heading h6">Site Navigation</h2>
+    <h2 class="header__nav-heading h6">
+        <?php
+        _e('Site Navigation','philosophy');
+        ?>
+    </h2>
 
     <?php
     wp_nav_menu(array(
@@ -13,6 +21,10 @@
     )
     ?>
 
-    <a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu">Close</a>
+    <a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu">
+        <?php
+        _e('Close','philosophy');
+        ?>
+    </a>
 
 </nav> <!-- end header__nav-wrap -->
